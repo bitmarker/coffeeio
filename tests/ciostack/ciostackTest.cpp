3b5db85c07b0334882e12e5cad3a4786ff62a66c
Leonid Lezner <info@bitmarker.de>
2016-07-26 08:02:30 +0200

    Writing tests for variants

--- tests/ciostack/ciostackTest.cpp
+++ tests/ciostack/ciostackTest.cpp
@@ -84,6 +84,89 @@ TEST(ciostack, buffer_object)
 	CHECK_EQUAL(offset, bufferObject.offset);
 }
 
+TEST(ciostack, put_bytes)
+{
+	long u32 = 0;
+	unsigned char buffer[32];
+    CIO_BUFFER_OBJECT bufferObject;
+    CIO_Buffer_Init(&bufferObject, buffer, sizeof(buffer));
+
+	CIO_Buffer_PutByte(&bufferObject, 0x12);
+	CIO_Buffer_PutByte(&bufferObject, 0x34);
+	CIO_Buffer_PutByte(&bufferObject, 0x56);
+	CIO_Buffer_PutByte(&bufferObject, 0xAB);
+	
+	CHECK_EQUAL(4, bufferObject.offset);
+
+	CIO_Buffer_Reset(&bufferObject);
+
+	CIO_Buffer_DeserializeLong(&bufferObject, &u32);
+	CHECK_EQUAL(0x123456AB, (unsigned long)u32);
+}
+
+TEST(ciostack, variants)
+{
+	COFFEEIO_VARIANT var1;
+	unsigned int offset = 0;
+	unsigned char buffer[32];
+    CIO_BUFFER_OBJECT bufferObject;
+    CIO_Buffer_Init(&bufferObject, buffer, sizeof(buffer));
+
+	var1.type = variant_none;
+	CHECK_EQUAL(0, CIO_VariantSize(&var1));
+
+	var1.type = variant_u8;
+	var1.value.u8 = 0xAB;
+	CIO_Buffer_SerializeVariant(&bufferObject, &var1);
+	CHECK_EQUAL(1, CIO_VariantSize(&var1));
+	CHECK_EQUAL(var1.value.u8, buffer[offset++]);
+
+	var1.type = variant_s8;
+	var1.value.s8 = -15;
+	CIO_Buffer_SerializeVariant(&bufferObject, &var1);
+	CHECK_EQUAL(1, CIO_VariantSize(&var1));
+	CHECK_EQUAL(var1.value.s8, (signed char)buffer[offset++]);
+
+	var1.type = variant_u16;
+	var1.value.u16 = 0xEF15;
+	CIO_Buffer_SerializeVariant(&bufferObject, &var1);
+	CHECK_EQUAL(2, CIO_VariantSize(&var1));
+	CHECK_EQUAL(0xEF, buffer[offset++]);
+	CHECK_EQUAL(0x15, buffer[offset++]);
+
+	var1.type = variant_s16;
+	var1.value.s16 = 0xCDAB;
+	CIO_Buffer_SerializeVariant(&bufferObject, &var1);
+	CHECK_EQUAL(2, CIO_VariantSize(&var1));
+	CHECK_EQUAL(0xCD, buffer[offset++]);
+	CHECK_EQUAL(0xAB, buffer[offset++]);
+
+	var1.type = variant_u32;
+	var1.value.u32 = 0x12345678;
+	CIO_Buffer_SerializeVariant(&bufferObject, &var1);
+	CHECK_EQUAL(4, CIO_VariantSize(&var1));
+	CHECK_EQUAL(0x12, buffer[offset++]);
+	CHECK_EQUAL(0x34, buffer[offset++]);
+	CHECK_EQUAL(0x56, buffer[offset++]);
+	CHECK_EQUAL(0x78, buffer[offset++]);
+
+	var1.type = variant_s32;
+	var1.value.s32 = 0xFA107435;
+	CIO_Buffer_SerializeVariant(&bufferObject, &var1);
+	CHECK_EQUAL(4, CIO_VariantSize(&var1));
+	CHECK_EQUAL(0xFA, buffer[offset++]);
+	CHECK_EQUAL(0x10, buffer[offset++]);
+	CHECK_EQUAL(0x74, buffer[offset++]);
+	CHECK_EQUAL(0x35, buffer[offset++]);
+
+	var1.type = variant_f32;
+	var1.value.f32 = 54.765;
+	CIO_Buffer_SerializeVariant(&bufferObject, &var1);
+	CHECK_EQUAL(4, CIO_VariantSize(&var1));
+	
+	
+}
+
 TEST(ciostack, init_frame_init)
 {
 	CIO_FRAME frameIO;

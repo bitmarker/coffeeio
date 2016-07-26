fe613c1ebfa3dad224e5b6814ffe24bf72e406db
Leonid Lezner <info@bitmarker.de>
2016-07-26 16:02:23 +0200

    Writing tests for the frame

--- tests/ciostack/ciostackTest.cpp
+++ tests/ciostack/ciostackTest.cpp
@@ -160,11 +160,13 @@ TEST(ciostack, variants)
 	CHECK_EQUAL(0x35, buffer[offset++]);
 
 	var1.type = variant_f32;
-	var1.value.f32 = 54.765;
+	var1.value.f32 = 12.3456;
 	CIO_Buffer_SerializeVariant(&bufferObject, &var1);
 	CHECK_EQUAL(4, CIO_VariantSize(&var1));
-	
-	
+	CHECK_EQUAL(0x41, buffer[offset++]);
+	CHECK_EQUAL(0x45, buffer[offset++]);
+	CHECK_EQUAL(0x87, buffer[offset++]);
+	CHECK_EQUAL(0x94, buffer[offset++]);	
 }
 
 TEST(ciostack, init_frame_init)
@@ -184,6 +186,7 @@ TEST(ciostack, frame_to_buffer)
     CIO_BUFFER_OBJECT bufferObject;
 	CIO_FRAME frameIO;
 	COFFEEIO_VARIANT fieldsIO[5];
+	unsigned int offset = 0;
 
     CIO_Buffer_Init(&bufferObject, buffer, sizeof(buffer));
 	CIO_FrameInit(&frameIO, fieldsIO, sizeof(fieldsIO) / sizeof(fieldsIO[0]));
@@ -191,19 +194,56 @@ TEST(ciostack, frame_to_buffer)
 	frameIO.header.flags.byte = 0x12;
 
 	frameIO.fields[0].type = variant_u32;
-	frameIO.fields[0].value.u32 = 123;
+	frameIO.fields[0].value.u32 = 0x0A1BF123;
 
 	frameIO.fields[1].type = variant_u32;
-	frameIO.fields[1].value.u32 = 987654321;
+	frameIO.fields[1].value.u32 = 0x98765432;
 
 	frameIO.fields[2].type = variant_u16;
-	frameIO.fields[2].value.u16 = 1234;
+	frameIO.fields[2].value.u16 = 0x1234;
 
 	frameIO.fields[3].type = variant_f32;
-	frameIO.fields[3].value.f32 = 12.532;
+	frameIO.fields[3].value.f32 = 12.3456;
 
-	frameIO.fields[4].type = variant_f32;
-	frameIO.fields[4].value.f32 = 15.5;
+	frameIO.fields[4].type = variant_u32;
+	frameIO.fields[4].value.u32 = 0x2468ABCD;
+
 
 	CIO_FrameToBuffer(&frameIO, &bufferObject);
+	
+	CHECK_EQUAL(4 + 4 + 4 + 2 + 4 + 4, bufferObject.offset);
+
+	// Start byte
+	CHECK_EQUAL(0xAA, buffer[offset++]);	
+
+	// Size (start bytes + size + flags + payload + crc)
+	CHECK_EQUAL(22, buffer[offset++]);
+	
+	// Flags
+	CHECK_EQUAL(0x12, buffer[offset++]);	
+	
+	// Payload	
+	// u32
+	CHECK_EQUAL(0x0A, buffer[offset++]);
+	CHECK_EQUAL(0x1B, buffer[offset++]);
+	CHECK_EQUAL(0xF1, buffer[offset++]);	
+	CHECK_EQUAL(0x23, buffer[offset++]);
+	// u32
+	CHECK_EQUAL(0x98, buffer[offset++]);	
+	CHECK_EQUAL(0x76, buffer[offset++]);
+	CHECK_EQUAL(0x54, buffer[offset++]);
+	CHECK_EQUAL(0x32, buffer[offset++]);
+	// u16
+	CHECK_EQUAL(0x12, buffer[offset++]);
+	CHECK_EQUAL(0x34, buffer[offset++]);
+	// f32
+	CHECK_EQUAL(0x41, buffer[offset++]);
+	CHECK_EQUAL(0x45, buffer[offset++]);
+	CHECK_EQUAL(0x87, buffer[offset++]);
+	CHECK_EQUAL(0x94, buffer[offset++]);	
+	// u32
+	CHECK_EQUAL(0x24, buffer[offset++]);	
+	CHECK_EQUAL(0x68, buffer[offset++]);
+	CHECK_EQUAL(0xAB, buffer[offset++]);
+	CHECK_EQUAL(0xCD, buffer[offset++]);
 }
